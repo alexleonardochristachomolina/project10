2e1746e98d23cd86e84e0e2dd579665b650133b3
Jareth Guerrero <yaredguerrero@gmail.com>
2023-12-08 20:36:42 -0600

    style: corriendo: npm run lint:fix para hacer PR

--- client/src/components/CardRecomendados/index.jsx
+++ client/src/components/CardRecomendados/index.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 export const CardRecomendados = () => {
-    return (
-        <div className='container w-[220px] h-[310px] flex flex-col items-center justify-start pt-[10px] gap-3'>
-            <div className='w-[200px] h-[200px] flex items-center justify-center'>
-                <img className='w-[95%] h-[95%] ' src="https://m.media-amazon.com/images/I/61h1JmIy4UL._AC_SX679_.jpg" alt="producto" />
-            </div>
-            <div>
-                <h4>$99.99</h4>
-                <p className='w-[186px] h-[53px] text-gray-500 text-base font-medium overflow-hidden'>
-                    detalles del producto, Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident corporis.
-                </p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="container w-[220px] h-[310px] flex flex-col items-center justify-start pt-[10px] gap-3">
+      <div className="w-[200px] h-[200px] flex items-center justify-center">
+        <img
+          className="w-[95%] h-[95%] "
+          src="https://m.media-amazon.com/images/I/61h1JmIy4UL._AC_SX679_.jpg"
+          alt="producto"
+        />
+      </div>
+      <div>
+        <h4>$99.99</h4>
+        <p className="w-[186px] h-[53px] text-gray-500 text-base font-medium overflow-hidden">
+          detalles del producto, Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Provident corporis.
+        </p>
+      </div>
+    </div>
+  );
+};
