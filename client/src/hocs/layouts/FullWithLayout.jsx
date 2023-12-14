9e0acacd0ea8fd33bd30af58c74a666909041648
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-12-14 12:03:17 -0300

    fix: Moviendo componente Slider al Home fix: Corregida key offers, se reemplazo el key title por el id, para evitar que se repitan

--- client/src/hocs/layouts/FullWithLayout.jsx
+++ client/src/hocs/layouts/FullWithLayout.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import SliderHome from '../../components/Slider';
 
 export const FullWithLayout = () => {
   return (
     <div className="bg-[#F7FAFC]">
       <Header />
-      <SliderHome />
       <Outlet />
       <Footer />
     </div>
