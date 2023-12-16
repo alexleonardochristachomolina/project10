4a0d8ea2e5e61befe6f709a77b2b03b76feb302f
jfpanchi <jfpanchi2@espe.edu.ec>
2023-12-15 20:17:45 -0500

    feat: :sparkles: Add VerticalMenu Categories
    Added VerticalMenu for All Categories Menu

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HambuergerIcon } from '../../assets/header-icons';
 import Slider from 'react-slick';
 import { ExpandMoreIcon } from '../../assets/control-icons';
+import VerticalMenu from '../VerticalMenu';
 
 const Navbar = () => {
+  const [isCategoriesMenuVisible, setCategoriesMenuVisible] = useState(false);
+  const toggleCategoriesMenu = () => {
+    setCategoriesMenuVisible(!isCategoriesMenuVisible);
+  };
+  const itemsCategoriesMenu = [
+    { category: 'Smartphones', href: '#' },
+    { category: 'Accessories', href: '#' },
+    { category: 'Cases', href: '#' },
+    { category: 'Chargers', href: '#' },
+    { category: 'Headphones', href: '#' },
+    { category: 'Wearable Devices', href: '#' },
+    { category: 'Tablets', href: '#' },
+  ];
   const settings = {
     dots: false,
     arrows: false,
@@ -12,17 +26,41 @@ const Navbar = () => {
     slidesToShow: 3,
     slidesToScroll: 3,
     autoplay: false,
+    responsive: [
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 330,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <section className="text-gray-900 font-medium body-font border-0 sm:border-t-2 sm:border-b-2 border-gray-200">
       <div className="container p-4 hidden sm:flex md:flex lg:flex xl:flex mx-auto items-start">
         <nav className="flex items-center text-base">
-          <a className="mr-5 hover:text-gray-500 active:text-blue-500" href="#">
+          <button
+            className="mr-5 hover:text-gray-500 active:text-blue-500 relative"
+            onClick={toggleCategoriesMenu}
+          >
             <div className="flex">
               <HambuergerIcon /> <span className="ml-1">All category</span>
             </div>
-          </a>
+            <VerticalMenu
+              itemsMenu={itemsCategoriesMenu}
+              isVerticalMenuVisible={isCategoriesMenuVisible}
+              toggleVerticalMenu={toggleCategoriesMenu}
+            />
+          </button>
           <a className="mr-5 hover:text-gray-500 active:text-blue-500" href="#">
             Hot offers
           </a>
@@ -43,8 +81,16 @@ const Navbar = () => {
         <nav className="p-4 pr-0">
           <Slider {...settings}>
             <div>
-              <div className="p-2 mr-2 bg-gray-200 active:bg-blue-500 rounded-md active:text-black text-blue-400 text-center">
-                <a href="#">All category</a>
+              <div
+                className="p-2 mr-2 bg-gray-200 active:bg-blue-500 rounded-md active:text-black text-blue-400 text-center relative"
+                onClick={toggleCategoriesMenu}
+              >
+                <button>All category</button>
+                <VerticalMenu
+                  itemsMenu={itemsCategoriesMenu}
+                  isVerticalMenuVisible={isCategoriesMenuVisible}
+                  toggleVerticalMenu={toggleCategoriesMenu}
+                />
               </div>
             </div>
             <div>
