24c0580a5063c75a68fbeb98ccf8071d3115e74e
Adan. J <117919468+adanj27@users.noreply.github.com>
2023-12-21 12:05:19 -0400

    Update FullWithLayout.jsx

--- client/src/hocs/layouts/FullWithLayout.jsx
+++ client/src/hocs/layouts/FullWithLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import SliderHome from '../../components/Home/Slider';
+import SliderHome from '../../components/Home/SliderHome';
 import Navbar from '../../components/Navbar';
 
 export const FullWithLayout = () => {
