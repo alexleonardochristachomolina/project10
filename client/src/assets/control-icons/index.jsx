638ca2e2bc59963244a31aa44ee290cfef275654
A. J <adanj270594@gmail.com>
2023-12-30 20:36:26 -0400

    feat: :sparkles: add products cards, store

--- client/src/assets/control-icons/index.jsx
+++ client/src/assets/control-icons/index.jsx
@@ -69,12 +69,84 @@ export const CloseIcon = ({ className, ...props }) => (
     viewBox="0 0 20 20"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    className={classNames('close-icon', className)}
+    className={classNames(className)}
     {...props}
   >
     <path
       d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
-      fill="#8B96A5"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+// eslint-disable-next-line react/prop-types
+export const FavoriteBorderIcon = ({ className, ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    className={classNames(className)}
+    {...props}
+  >
+    <path
+      d="M16.5 2.82495C14.76 2.82495 13.09 3.63495 12 4.91495C10.91 3.63495 9.24 2.82495 7.5 2.82495C4.42 2.82495 2 5.24495 2 8.32495C2 12.105 5.4 15.185 10.55 19.865L12 21.175L13.45 19.855C18.6 15.185 22 12.105 22 8.32495C22 5.24495 19.58 2.82495 16.5 2.82495ZM12.1 18.375L12 18.475L11.9 18.375C7.14 14.065 4 11.215 4 8.32495C4 6.32495 5.5 4.82495 7.5 4.82495C9.04 4.82495 10.54 5.81495 11.07 7.18495H12.94C13.46 5.81495 14.96 4.82495 16.5 4.82495C18.5 4.82495 20 6.32495 20 8.32495C20 11.215 16.86 14.065 12.1 18.375Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+// eslint-disable-next-line react/prop-types
+export const StarBorderIcon = ({ className, ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    className={classNames(className)}
+    {...props}
+  >
+    <path
+      d="M22 9.74L14.81 9.12L12 2.5L9.19 9.13L2 9.74L7.46 14.47L5.82 21.5L12 17.77L18.18 21.5L16.55 14.47L22 9.74ZM12 15.9L8.24 18.17L9.24 13.89L5.92 11.01L10.3 10.63L12 6.6L13.71 10.64L18.09 11.02L14.77 13.9L15.77 18.18L12 15.9Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+// eslint-disable-next-line react/prop-types
+export const StarHalfIcon = ({ className, ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    className={classNames(className)}
+    {...props}
+  >
+    <path
+      d="M22 9.74L14.81 9.12L12 2.5L9.19 9.13L2 9.74L7.46 14.47L5.82 21.5L12 17.77L18.18 21.5L16.55 14.47L22 9.74ZM12 15.9V6.6L13.71 10.64L18.09 11.02L14.77 13.9L15.77 18.18L12 15.9Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+// eslint-disable-next-line react/prop-types
+export const StarIcon = ({ className, ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    className={classNames(className)}
+    {...props}
+  >
+    <path
+      d="M12 17.77L18.18 21.5L16.54 14.47L22 9.74L14.81 9.13L12 2.5L9.19 9.13L2 9.74L7.46 14.47L5.82 21.5L12 17.77Z"
+      fill="currentColor"
     />
   </svg>
 );
--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ExpandLessIcon } from '../assets/control-icons';
 import ProductList from '../components/Products/ProductList';
+import SidebarFilter from '../components/SidebarFilter';
 
 const Products = () => {
   const categories = ['Home', 'Clothings', "Men's wear", 'Summer clothing'];
   return (
-    <div className="container max-w-7xl mx-auto flex flex-col gap-5">
+    <div className="container max-w-6xl mx-auto flex flex-col gap-5">
       <div className="flex text-[#8B96A5] my-5">
         {categories.map((cat, i) => (
           <div key={i} className="flex">
@@ -18,7 +19,8 @@ const Products = () => {
           </div>
         ))}
       </div>
-      <div>
+      <div className="flex gap-5">
+        <SidebarFilter />
         <ProductList />
       </div>
     </div>
