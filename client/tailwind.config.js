421df977a6fd28602716bafb6ab5a9b036ad9e4a
alex <alex@gmail.com>
2024-01-05 23:47:13 +0900

    customize style

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const style=require('./src/assets/global/style.css');
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
