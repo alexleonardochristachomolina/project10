b595da5af72ff53d8effabc1bbb93516c2263008
A. J <adanj270594@gmail.com>
2024-01-04 20:08:41 -0400

    feat: :sparkles: add checkout components

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -3,13 +3,15 @@ import BannerDiscount from '../components/BannerDiscount';
 import { SavedOrLatherSection } from '../components/SavedOrLatherSection/SavedOrLatherSection';
 import CartSection from '../components/Cart/CartSection';
 import { Services } from '../components/Services';
+import Checkout from '../components/Cart/Checkout';
 
 const Cart = () => {
   return (
     <div className="container max-w-6xl mx-auto flex flex-col gap-5 my-5">
       <p>My cart (3)</p>
-      <div>
+      <div className="flex gap-5">
         <CartSection />
+        <Checkout />
       </div>
       <Services />
       <SavedOrLatherSection />
