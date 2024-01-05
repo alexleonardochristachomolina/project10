c6e4128d8d6d1b6bc04b7cae4683d9b2433a4277
A. J <adanj270594@gmail.com>
2024-01-05 09:38:05 -0400

    feat: :sparkles: add subscription section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,17 +5,21 @@ import SectionRecomendados from '../components/Home/SectionRecomendados';
 import { CategoriesSection } from '../components/Home/CategoriesSection';
 import { SecondCategories } from '../components/Home/SecondCategories';
 import Banner from '../components/Banner';
+import SubscriptionSection from '../components/Home/SubscriptionSection';
 
 const Home = () => {
   return (
-    <div className="container max-w-7xl mx-auto flex flex-col gap-5">
-      <Banner />
-      <DiscountOffersSection />
-      <CategoriesSection />
-      <SecondCategories />
-      <SliderHome />
-      <SectionRecomendados />
-    </div>
+    <>
+      <div className="container max-w-7xl mx-auto flex flex-col gap-5">
+        <Banner />
+        <DiscountOffersSection />
+        <CategoriesSection />
+        <SecondCategories />
+        <SliderHome />
+        <SectionRecomendados />
+      </div>
+      <SubscriptionSection />
+    </>
   );
 };
 
--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -2,28 +2,32 @@ import React from 'react';
 import { ExpandLessIcon } from '../assets/control-icons';
 import ProductList from '../components/Products/ProductList';
 import SidebarFilter from '../components/SidebarFilter';
+import SubscriptionSection from '../components/Home/SubscriptionSection';
 
 const Products = () => {
   const categories = ['Home', 'Clothings', "Men's wear", 'Summer clothing'];
   return (
-    <div className="container max-w-6xl mx-auto flex flex-col gap-5">
-      <div className="flex text-[#8B96A5] my-5">
-        {categories.map((cat, i) => (
-          <div key={i} className="flex">
-            <span>{cat}</span>
-            {i < categories.length - 1 && (
-              <span className="transform rotate-90">
-                <ExpandLessIcon />
-              </span>
-            )}
-          </div>
-        ))}
+    <>
+      <div className="container max-w-6xl mx-auto flex flex-col gap-5">
+        <div className="flex text-[#8B96A5] my-5">
+          {categories.map((cat, i) => (
+            <div key={i} className="flex">
+              <span>{cat}</span>
+              {i < categories.length - 1 && (
+                <span className="transform rotate-90">
+                  <ExpandLessIcon />
+                </span>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="flex gap-5">
+          <SidebarFilter />
+          <ProductList />
+        </div>
       </div>
-      <div className="flex gap-5">
-        <SidebarFilter />
-        <ProductList />
-      </div>
-    </div>
+      <SubscriptionSection />
+    </>
   );
 };
 
