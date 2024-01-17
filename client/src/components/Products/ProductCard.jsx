59cbbfcf142d9d14d89836dd379dcff93322d2c9
Emanuel Hermosilla <hermosilla.emanuel811@gmail.com>
2024-01-17 10:20:27 -0300

    feat(Products): :iphone: Add mobile version of the products page

--- client/src/components/Products/ProductCard.jsx
+++ client/src/components/Products/ProductCard.jsx
@@ -11,16 +11,22 @@ import { Link } from 'react-router-dom';
 export const ProductCard = ({ product }) => {
   const { id, image, price, brand, model } = product;
   return (
-    <div className="bg-white border rounded-lg w-72">
-      <div className="border-b py-8 px-12">
-        <div className="w-52 h-52 flex justify-center">
-          <img src={image[0]} alt="" />
-        </div>
+    <div className="flex items-center border rounded-lg max-w-sm sm:block">
+      <div className="bg-white sm:border-b py-8 px-10">
+        <img
+          className="object-scale-down aspect-square w-full"
+          src={image[0]}
+          alt={model}
+        />
       </div>
-      <div className="flex gap-3 p-5 justify-between">
+
+      <div className="flex gap-3 p-3 justify-between">
         <div className="flex flex-col ">
-          <span className="text-[18px] font-semibold">
-            ${price} <span className="text-[#8B96A5] text-base">$1128.00</span>
+          <span className="text-[18px] font-semibold text-gray-600 sm:order-first">
+            ${price}{' '}
+            <span className="text-[#8B96A5] text-base line-through">
+              $1128.00
+            </span>
           </span>
           <div className="text-[#FF9017] flex gap-1 my-1">
             <div className="flex">
@@ -32,13 +38,16 @@ export const ProductCard = ({ product }) => {
             </div>
             7.5
           </div>
-          <Link to={`/products/${id}`}>
+          <Link to={`/products/${id}`} className="order-first sm:order-last">
             <h2 className="text-[#606060] hover:underline">
               {brand} {model}
             </h2>
           </Link>
+          <span className="sm:hidden text-green-600 text-xs">
+            Free Shipping
+          </span>
         </div>
-        <div>
+        <div className="hidden sm:block">
           <FavoriteBorderIcon className="border w-10 h-10 p-2 rounded-md hover:bg-[#0D6EFD] cursor-pointer text-[#0D6EFD] hover:text-white transition-colors" />
         </div>
       </div>
