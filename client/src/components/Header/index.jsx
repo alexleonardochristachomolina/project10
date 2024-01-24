add38a3de86067813d95dcaae81b27cd520ab1e2
Jareth Guerrero <yaredguerrero@gmail.com>
2024-01-24 10:31:25 -0600

    feat: :sparkles: Links de redirección agregados al header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -24,14 +24,14 @@ const Header = () => {
           <InputSearch />
         </div>
         <div className="flex items-center gap-4">
-          <div className="flex flex-col items-center">
+        <Link to='/cart' className="flex flex-col items-center">
             <ShoppingCartIcon />
             <p className="hidden md:inline md:text-gray-500">My cart</p>
-          </div>
-          <div className="flex flex-col items-center">
+          </Link>
+          <Link to='/auth' className="flex flex-col items-center">
             <PersonIcon />
             <p className="hidden md:inline md:text-gray-500">Profile</p>
-          </div>
+          </Link>
         </div>
       </div>
       <div className="md:hidden mt-4">
