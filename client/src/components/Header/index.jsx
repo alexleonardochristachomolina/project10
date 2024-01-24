2a691d7acfe995fabfb09fce8cf94782ada41fae
Jareth Guerrero <yaredguerrero@gmail.com>
2024-01-24 10:34:36 -0600

    Lint:Fix

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -22,11 +22,11 @@ const Header = () => {
           <InputSearch />
         </div>
         <div className="flex items-center gap-4">
-        <Link to='/cart' className="flex flex-col items-center">
+          <Link to="/cart" className="flex flex-col items-center">
             <ShoppingCartIcon />
             <p className="hidden md:inline md:text-gray-500">My cart</p>
           </Link>
-          <Link to='/auth' className="flex flex-col items-center">
+          <Link to="/auth" className="flex flex-col items-center">
             <PersonIcon />
             <p className="hidden md:inline md:text-gray-500">Profile</p>
           </Link>
