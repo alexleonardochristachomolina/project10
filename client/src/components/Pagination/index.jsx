3c0be6846410a170e3e8f1fad648fa9f78fdaf9c
Emanuel Hermosilla <hermosilla.emanuel811@gmail.com>
2024-02-07 23:07:26 -0300

    feat: :sparkles: Add dynamic product listing

--- client/src/components/Pagination/index.jsx
+++ client/src/components/Pagination/index.jsx
@@ -1,25 +1,19 @@
-import React, { useState } from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react';
 import { ChevronLeft, ChevronRight } from '../../assets/left-right-chevron';
 
-export const Pagination = () => {
-  const pageDefault = 1;
-  const [num, setNum] = useState(pageDefault);
-  const [cur, setCur] = useState(pageDefault);
-
-  const pages = [
-    { page: num },
-    { page: num + 1 },
-    { page: num + 2 },
-    { page: num + 3 },
-  ];
-
-  const Next = () => {
-    setNum(num + 1);
-  };
-
-  const Previous = () => {
-    num > 1 && setNum(num - 1);
-  };
+export const Pagination = ({
+  currentPage,
+  setCurrentPage,
+  nextPage,
+  prevPage,
+  totalProducts,
+  productsPerPage,
+}) => {
+  const pages = [];
+  for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+    pages.push(i);
+  }
 
   return (
     <>
@@ -44,31 +38,37 @@ export const Pagination = () => {
             aria-label="Pagination"
           >
             <button
-              onClick={Previous}
+              onClick={() => {
+                currentPage > 1 && prevPage();
+              }}
               className="rounded-l-md px-3 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Anterior</span>
               <ChevronLeft />
             </button>
 
-            {pages.map((e, index) => (
+            {pages.map((page, index) => (
               <button
                 key={index}
-                onClick={() => setCur(e.page)}
+                onClick={() => {
+                  setCurrentPage(page);
+                }}
                 className={`relative w-11 py-2 text-base font-medium text-[#1C1C1C] ring-1 ring-inset ring-gray-300 
                 ${
-                  cur === e.page
+                  currentPage === page
                     ? 'bg-[#DEE2E7] text-[#8B96A5]'
                     : 'hover:bg-gray-100 focus:z-20 focus:outline-offset-0'
                 }
                 `}
               >
-                {e.page}
+                {page}
               </button>
             ))}
 
             <button
-              onClick={Next}
+              onClick={() => {
+                currentPage <= pages.length - 1 && nextPage();
+              }}
               className="rounded-r-md px-3 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Siguiente</span>
